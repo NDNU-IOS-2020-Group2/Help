72ebf504c9b331608066cc13380583edeb26940e
jorgesv16 <jorge@github.com>
2020-05-02 11:15:19 -0700

    Implemented help button and profile page

--- Help/gpsViewController 2.swift
+++ Help/gpsViewController 2.swift
@@ -12,12 +12,11 @@ import MapKit
 class gpsViewController: UIViewController, CLLocationManagerDelegate {
     var locationManager = CLLocationManager()
     
-    @IBOutlet weak var map: MKMapView!
     
-//    @IBOutlet weak var map: MKMapView!
+    @IBOutlet var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.locationManager.requestWhenInUseAuthorization()
         // Do any additional setup after loading the view.
         if CLLocationManager.locationServicesEnabled() {
@@ -34,26 +33,5 @@ class gpsViewController: UIViewController, CLLocationManagerDelegate {
         let userLocation = locations.last
         let viewRegion = MKCoordinateRegion.init(center: (userLocation?.coordinate)!, latitudinalMeters: 600, longitudinalMeters: 600)
         self.map.setRegion(viewRegion, animated: true)
-        
-        addAnnotations()
-    }
-    
-    func addAnnotations() {
-        
-        let doctor = MKPointAnnotation()
-        doctor.title = "Doctor"
-        doctor.coordinate = CLLocationCoordinate2D(latitude: 37.786430 , longitude:-122.408094)
-        
-        let nurse = MKPointAnnotation()
-        nurse.title = "Nurse"
-        nurse.coordinate = CLLocationCoordinate2D(latitude: 37.788693 , longitude: -122.405286)
-        
-        let emt = MKPointAnnotation()
-        emt.title = "EMT"
-        emt.coordinate = CLLocationCoordinate2D(latitude: 37.784530 , longitude: -122.404337)
-        
-        map.addAnnotation(doctor)
-        map.addAnnotation(nurse)
-        map.addAnnotation(emt)
     }
 }
